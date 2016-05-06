832d26350c8c4bbe8633a4871e783be233b16382
Ryan Sobol <contact@ryansobol.com>
2016-05-06 09:24:00 -0700

    Sync part2 bonus with solutions

--- part2/bonus.js
+++ part2/bonus.js
@@ -1,48 +1,87 @@
-// Define a function named group that takes in two arguments:
-//     arr (an array of objects)
-//     key (string)
+// Define a function named bigSum that takes in an arbitrary number of
+// arguments. Assume all arguments are a number.
 //
-// Returns an object containing collections grouped by the value of each object
-// at that key
+// Return the sum of all of the arguments. For example, given 1, 2, 3, and 4,
+// then return 10. If there are no arguments, return 0.
 //
-// Example:
-// var cards = [
-//   {name: 'ace', suit: 'spades'},
-//   {name: '2', suit: 'spades'},
-//   ...
-//   {name: 'ace', suit: 'hearts'},
-//   ...
-// ]
-//
-// group(cards, 'suit') ->
-//   {
-//     spades: [{name: 'ace', suit: 'spades'}, {name: '2', suit: 'spades'} ...]
-//     hearts: [{name: 'ace', suit: 'hearts'}, ...]
-//     diamonds: [{name: 'ace', suit: 'diamonds'}, ...]
-//     clubs: [{name: 'ace', suit: 'clubs'}, ...]
-//   }
+// See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/arguments
+function bigSum() {
+  // YOUR CODE HERE
+}
 
-
-
-// Define a function named merge that takes in two arguments:
-//    arr1 (sorted array of numbers)
-//    arr2 (sortedarray of numbers)
+// Define a function named merge that takes in two arguments.
+//    arr1 (array of sorted numbers)
+//    arr2 (array of sorted numbers)
 //
 // Return a new array that contains all the numbers in each array still sorted.
-// Example: given [1, 3, 5] and [2, 4], it would produce [1, 2, 3, 4, 5]
-// Another example: given [1, 3, 5], [2, 2], it would produce [1, 2, 2, 3, 5]
+// For example, given [1, 3, 5] and [2, 4], then return [1, 2, 3, 4, 5].
+
+
+
+
+// Define a function named unique that takes in one argument.
+//    arr (array of strings)
 //
-// Tip: Each array may contain duplicate numbers. You need to ensure that each
-// number is still accounted for.
-// Another Tip: Each array is sorted, which makes the solution easier.
+// Return a new array that contains all the elements of arr minus any
+// duplicates. For example, given ['a', 'b', 'a', 'c'], then return
+// ['a', 'b', 'c'].
 
 
 
-// Define a function named bigSum that takes in an arbitrary number of
-// parameters. We can assume all parameters are numbers.
+
+// In the early days of Roman numerals, the Romans didn't bother with any of
+// this new-fangled subtraction 'IX' nonsense. No sir, it was straight addition,
+// biggest to littlest. So 9 was written 'VIIII' and so on.
 //
-// Returns the sum of all of the parameters. For example
-// bigSum(1, 2, 3, 4) produces 10.
-// If there are no parameters, return 0.
-// Tip: Think about how this function is different from the sum function in
-// the assignment.
+// Define a function called oldSchoolRomanNumeral that takes in one argument.
+//     num (number between 1 and 1000)
+//
+// Return a string that represents the roman numeral in the old school format.
+// For example, given 9, then return 'VIIII'.
+//
+// TIP #1: Here's a mapping of Roman to Arabic numerals.
+//
+//   1000 = M
+//    500 = D
+//    100 = C
+//     50 = L
+//     10 = X
+//      5 = V
+//      1 = I
+//
+// TIP #2: The Math.floor() and remainder functions will be helpful.
+//
+// See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/repeat
+
+
+
+
+// Eventually, someone thought it would be terribly clever if putting a smaller
+// number before a larger one meant you had to subtract the smaller one. As a
+// result of this development, you must now suffer.
+//
+// Define a function called newSchoolRomanNumeral that takes in one argument.
+//     num (number between 1 and 1000)
+//
+// Return a string that represents the roman numeral in the new school format.
+// For example, given 9, then return 'IX'.
+//
+// TIP #1: Here's a mapping of Roman to Arabic numerals:
+//
+//   1000 = M
+//    900 = CM
+//    500 = D
+//    400 = CD
+//    100 = C
+//     90 = XC
+//     50 = L
+//     40 = XL
+//     10 = X
+//      9 = IX
+//      5 = V
+//      4 = IV
+//      1 = I
+//
+// TIP #2: The Math.floor() and remainder functions will be helpful.
+//
+// See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/repeat
--- part2/bonus.test.js
+++ part2/bonus.test.js
@@ -1,40 +1,12 @@
 window.onload = function() {
   var assert = chai.assert;
   mocha.setup('tdd');
-  suite('#group', function() {
-    test('groups empty arrays correctly', function() {
-      assert.deepEqual(group([], 'foo'), {});
-    });
 
-    var SUITS = ['spades', 'hearts', 'clubs', 'diamonds'];
-    var VALUES = ['A', 'K'];
-    var cards = [];
-    for (var i = 0; i < SUITS.length; i++) {
-      for (var j = 0; j < VALUES.length; j++) {
-        cards.push({
-          value: VALUES[j],
-          suit: SUITS[i]
-        });
-      }
-    }
-    var grouped = group(cards, 'suit');
-    test('produces the proper groups', function() {
-      assert.sameMembers(Object.keys(grouped), SUITS);
-    });
-
-    test('produces the proper members for each group', function() {
-      assert.sameDeepMembers(grouped.hearts, [
-        {value: 'A', suit: 'hearts'}, {value: 'K', suit: 'hearts'}
-      ]);
-      assert.sameDeepMembers(grouped.clubs, [
-        {value: 'A', suit: 'clubs'}, {value: 'K', suit: 'clubs'}
-      ]);
-      assert.sameDeepMembers(grouped.spades, [
-        {value: 'A', suit: 'spades'}, {value: 'K', suit: 'spades'}
-      ]);
-      assert.sameDeepMembers(grouped.diamonds, [
-        {value: 'A', suit: 'diamonds'}, {value: 'K', suit: 'diamonds'}
-      ]);
+  suite('#bigSum', function() {
+    test('adds a bunch of numbers as parameters', function() {
+      assert.strictEqual(bigSum(1, 2, 3, 4), 10);
+      assert.strictEqual(bigSum(1, -1, 2, -3), -1);
+      assert.strictEqual(bigSum(), 0);
     });
   });
 
@@ -44,14 +16,59 @@ window.onload = function() {
       assert.deepEqual(merge([1, 3, 5], [1, 2]), [1, 1, 2, 3, 5]);
       assert.deepEqual(merge([], [1, 2]), [1, 2]);
       assert.deepEqual(merge([1, 2], []), [1, 2]);
+      assert.deepEqual(merge([], []), []);
     });
   });
 
-  suite('#bigSum', function() {
-    test('adds a bunch of numbers as parameters', function() {
-      assert.strictEqual(bigSum(), 0);
-      assert.strictEqual(bigSum(1, 2, 3, 4), 10);
-      assert.strictEqual(bigSum(1, -1, 2, -3), -1);
+  suite('#unique', function() {
+    test('returns an array with only unique values', function() {
+      assert.sameMembers(unique([]), []);
+      assert.sameMembers(unique(['a', 'b', 'a', 'c']), ['a', 'b', 'c']);
+      assert.sameMembers(unique(['a', 'b', 'c']), ['a', 'b', 'c']);
+    });
+  });
+
+  suite('#oldSchoolRomanNumeral', function() {
+    test('converts a number to a roman numeral in the old school format', function() {
+      assert.strictEqual(oldSchoolRomanNumeral(1), 'I');
+      assert.strictEqual(oldSchoolRomanNumeral(2), 'II');
+      assert.strictEqual(oldSchoolRomanNumeral(4), 'IIII');
+      assert.strictEqual(oldSchoolRomanNumeral(5), 'V');
+      assert.strictEqual(oldSchoolRomanNumeral(6), 'VI');
+      assert.strictEqual(oldSchoolRomanNumeral(9), 'VIIII');
+      assert.strictEqual(oldSchoolRomanNumeral(10), 'X');
+      assert.strictEqual(oldSchoolRomanNumeral(11), 'XI');
+      assert.strictEqual(oldSchoolRomanNumeral(15), 'XV');
+      assert.strictEqual(oldSchoolRomanNumeral(16), 'XVI');
+      assert.strictEqual(oldSchoolRomanNumeral(52), 'LII');
+      assert.strictEqual(oldSchoolRomanNumeral(72), 'LXXII');
+      assert.strictEqual(oldSchoolRomanNumeral(72), 'LXXII');
+      assert.strictEqual(oldSchoolRomanNumeral(134), 'CXXXIIII');
+      assert.strictEqual(oldSchoolRomanNumeral(273), 'CCLXXIII');
+      assert.strictEqual(oldSchoolRomanNumeral(749), 'DCCXXXXVIIII');
+      assert.strictEqual(oldSchoolRomanNumeral(1000), 'M');
+    });
+  });
+
+  suite('#newSchoolRomanNumeral', function() {
+    test('converts a number to a roman numeral in the new school format', function() {
+      assert.strictEqual(newSchoolRomanNumeral(1), 'I');
+      assert.strictEqual(newSchoolRomanNumeral(2), 'II');
+      assert.strictEqual(newSchoolRomanNumeral(4), 'IV');
+      assert.strictEqual(newSchoolRomanNumeral(5), 'V');
+      assert.strictEqual(newSchoolRomanNumeral(6), 'VI');
+      assert.strictEqual(newSchoolRomanNumeral(9), 'IX');
+      assert.strictEqual(newSchoolRomanNumeral(10), 'X');
+      assert.strictEqual(newSchoolRomanNumeral(11), 'XI');
+      assert.strictEqual(newSchoolRomanNumeral(15), 'XV');
+      assert.strictEqual(newSchoolRomanNumeral(16), 'XVI');
+      assert.strictEqual(newSchoolRomanNumeral(52), 'LII');
+      assert.strictEqual(newSchoolRomanNumeral(72), 'LXXII');
+      assert.strictEqual(newSchoolRomanNumeral(72), 'LXXII');
+      assert.strictEqual(newSchoolRomanNumeral(134), 'CXXXIV');
+      assert.strictEqual(newSchoolRomanNumeral(273), 'CCLXXIII');
+      assert.strictEqual(newSchoolRomanNumeral(749), 'DCCXLIX');
+      assert.strictEqual(newSchoolRomanNumeral(1000), 'M');
     });
   });
 
