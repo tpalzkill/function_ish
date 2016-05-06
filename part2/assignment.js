fa702c7c721a7ec475c7a5eee9cfba34c0a71bbb
Ryan Sobol <contact@ryansobol.com>
2016-05-06 09:22:05 -0700

    Sync part2 assignment with solutions

--- part2/assignment.js
+++ part2/assignment.js
@@ -2,12 +2,14 @@
 //    arr (array of numbers)
 //
 // Return the sum of all of the numbers in the array. For example, given
-// [1, 2, 3, 4], it produces 10. In the event the array is empty. Return 0.
+// [1, 2, 3, 4], then return 10. If the array is empty, return 0.
 function sum(arr) {
   // var sum = 0;
-  // for(var i = 0; i < arr.length; i++) {
+  //
+  // for (var i = 0; i < arr.length; i++) {
   //   sum += arr[i];
   // }
+  //
   // return sum;
 }
 
@@ -15,226 +17,205 @@ function sum(arr) {
 //    arr (array of numbers)
 //
 // Return the product of all of the numbers in the array. For example, given
-// [1, 2, 3, 4] produces 24. In the event the array is empty. Return 1.
+// [1, 2, 3, 4], then return 24. If the array is empty, return 1.
 function product(arr) {
-
+  // YOUR CODE HERE
 }
 
 // Define a function named concatenate that takes in one argument.
 //    arr (array of strings)
 //
-// Return a string that combines the strings in the order of the array. For
-// example, ['hello', 'my', 'name', 'is', 'ken'] produces 'hellomynameisken'
+// Return the concatenation of all the strings in the array. For example, given
+// ['hello', 'my', 'name', 'is', 'ken'], then return 'hellomynameisken'. If the
+// array is empty, return ''.
 function concatenate(arr) {
 
 }
 
+// Define a function named repeat that takes in two arguments.
+//     input (string)
+//     integer (number)
+//
+// Return a new string containing integer copies of the input string. For
+// example, given 'hi' and 4, then return 'hihihihi'.
+function repeat(str, times) {
+
+}
+
+// Define a function named filterPassingGrades that takes in one argument.
+//     grades (array of numbers)
+//
+// Return a new array with any grade less than 70 filtered out. For example,
+// given [88, 67, 70, 92, 53], then return [88, 70, 92].
+
+
+
+
+// Define a function named replace that takes in three arguments.
+//    arr (array of numbers)
+//    from (number)
+//    to (number)
+//
+// Return a new array of numbers where all from elements are replaced with to.
+// For example, given [1, 3, 2, 1, 3], 1, and 4, then return [4, 3, 2, 4, 3].
+
+
+
+
+// Define a function named flatten that takes in one argument.
+//     arr (array of arrays)
+//
+// Return a new array that combines all of elements of each inner array. For
+// example, given [[1], [2, 3], [4]], then return [1, 2, 3, 4].
+//
+// Tip: You only need to flatten one level deep.
+//
+// See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/concat
+
+
+
+
 // Define a function named max that takes in one argument.
 //    arr (array of numbers)
 //
-// Return a the maximum number in the array. For example, given [1, 2, -3, 4], // it produces 4.
+// Return the maximum number in the array. For example, given [1, 2, -3, 4],
+// then return 4. If the array is empty, return -Infinity.
 //
-// Tip: Assume that if the array is empty, we use the -Infinity in JavaScript.
-// Tip: You might find Math.max handy.
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/max
+// See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/max
+
 
 
 
 // Define a function named min that takes in one argument.
 //    arr (array of numbers)
 //
-// Return a the minimum number in the array. For example, given [1, 2, -3, 4],
-// it produces -3.
+// Return the minimum number in the array. For example, given [1, 2, -3, 4],
+// then return -3. If the array is empty, return Infinity.
 //
-// Tip: Assume that if the array is empty, we use Infinity in JavaScript.
-// Tip: You might find Math.min handy.
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/min
+// See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/min
 
 
 
-// Define a function named mean that takes in one arguments
+
+// Define a function named mean that takes in one argument.
 //    arr (array of numbers)
 //
 // Return the mean (i.e. average) of all of the numbers in the array. For
-// example, given [1, 2], it produces 1.5.
-// Return null if the array is empty.
+// example, given [1, 2, 6], then return 3. If the array is empty, return null.
+
 
 
 
 // Define a function named median that takes in one argument.
 //    arr (array of numbers)
 //
-// Return the median of all of the numbers. Return null if the array is empty.
+// Return the median of all of the numbers. For example, given [1, 2, 6], then
+// return 2. Also, if given [1, 2, 6, 8], return 5. If the array is empty,
+// return null.
 //
-// Tip: Use google to find out how to calculate the median.
-// Another tip: The array may not be sorted. There is an easy method in
-// JavaScript called sort that can help you. See:
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
-
-
-
-// Define a function, distance, that takes in two arguments
-//    coord1: object
-//    coord2: object
-// Each argument has the following format:
-// {
-//   x: <NUMBER>,
-//   y: <NUMBER>
-// }
+// Tip: Use Google to learn more about calculating the median.
+// Tip: The given array may not be sorted.
 //
-// Return the distance between the two points on a graph.
-// Tip: Use google to find out how to calculate the distance between two points.
+// See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
 
 
 
-// Define a function named repeat that takes in two arguments
-//     str (string)
-//     times (positive number)
-//
-// Returns a string that is the input string repeated multiple times. For
-// example, given 'hi' and 4, it should produce 'hihihihi'
 
-
-
-// Define a function named contains that is given two arguments
+// Define a function named contains that takes in two arguments.
 //     arr (array of strings)
 //     str (string)
 //
-// Returns true if that string exists in the array, false otherwise.
+// Return true if that string exists in the array, otherwise false.
 
 
 
-// Define a function named replace that takes in three arguments
-//    arr (array of numbers)
-//    from (number)
-//    to (number)
+
+// Define a function named distance that takes in two arguments.
+//    point1 (object)
+//    point2 (object)
 //
-// Returns an array of numbers where all elements of from are replaced with to
-// Example:
-// replace([1, 3, 2, 1, 3], 1, 4) -> [4, 3, 2, 4, 3]
-
-
-
-// Define a function named filterPassingGrades that takes in one argument:
-//     grades (array of numbers)
+// Assume each point argument has the following format.
+//    { x: NUMBER, y: NUMBER }
 //
-// Return the array with only numbers greater than or equal to 70 (ie the
-// passing scores).
+// Return the distance between the two points on a Cartesian coordinate system.
+// For example, given { x: 3, y: 2 } and { x: 9, y: 7 }, then return
+// approximately 7.810249675906654.
+//
+// Tip: Use Google to learn more about calculating the distance.
 
 
 
-// Define a function named pluck that takes in two arguments:
+
+// Define a function named combine that takes in two arguments.
+//    obj1 (object)
+//    obj2 (object)
+//
+// Return a new object that has the key-value pairs of both objects. For
+// example, given { a: 1 } and { b: 2 }, then return { a: 1, b: 2 }.
+
+
+
+
+// Define a function called invert that takes in one argument.
+//    obj (object)
+//
+// Return a new object where the keys and values of the argument are inverted.
+// For example, given { a: 1, b: 2 }, then return { '1': 'a', '2': 'b' }.
+
+
+
+
+// Define a function named values that takes in one argument.
+//    obj (object)
+//
+// Return an array of the values of the object. For example, given
+// { a: 1, b: 2, c: 3 }, then return [1, 2, 3].
+
+
+
+
+// Define a function called toPairs that takes in one argument.
+//    obj (object)
+//
+// Return a new array where each element is key-value pair array of the
+// argument. For example, given { a: 1, b: 2 }, then return
+// [['a', 1], ['b', 2]].
+
+
+
+
+// Define a function called fromPairs that takes in one argument.
+//    arr (array)
+//
+// Return a new object where each key-value pair is a from an element in the
+// argument. For example, given [['a', 1], ['b', 2]], then return
+// { a: 1, b: 2 }.
+
+
+
+
+// Define a function named pluck that takes in two arguments.
 //     arr (array of objects)
 //     key (string)
 //
-// Returns an array of elements where each element is the value in each object.
+// Return a new array where each element is the keyed value of each object.
 //
-// Example:
-// var stooges = [
-//   {name: 'moe', age: 40},
-//   {name: 'larry', age: 50},
-//   {name: 'curly', age: 60}
-// ];
-// pluck(stooges, 'name') -> ['moe', 'larry', 'curly']
-
-
-
-// Define a function named flatten given one argument:
-//     arr (array of arrays)
+// For example, given the array:
+//    [
+//      { name: 'moe', age: 40 },
+//      { name: 'larry', age: 50 },
+//      { name: 'curly', age: 60 }
+//    ]
 //
-// Returns an array that combines all of the elements in the original arrays.
-//
-// Examples:
-//   flatten([[1], [2], [3], [4]]) -> [1, 2, 3, 4]
-//   flatten([[1], [2, 3], [4]]) -> [1, 2, 3, 4]
-// Tip: In the second example, we only need to flatten one level deep.
-//   flatten([[1], [2, [3]], [4]]) -> [1, 2, [3], 4]
+// and the key 'name', then return ['moe', 'larry', 'curly'].
 
 
 
-// Define a function named values that takes in one argument:
-//    obj (object)
-//
-// Returns an array of the values of the object. For example, given
-// {a: 1, b: 2, c: 3}, it produces [1, 2, 3]
 
-
-
-// Define a function named pick that takes in two arguments:
+// Define a function named pick that takes in two arguments.
 //    obj (object)
 //    keys (array of strings)
 //
-// Returns an object that only contains the keys specified.
-//
-// Example:
-// pick({name: 'moe', age: 50, userid: 'moe1'}, ['name', 'age'])
-//   -> {name: 'moe', age: 50}
-
-
-
-// Define a function named unique that takes in one argument:
-//    arr (array of strings)
-//
-// Return an array that contains each of the elements in the array but
-// removes any duplicate elements.
-//
-// Example:
-//   unique(['a', 'b', 'a', 'c']) -> ['a', 'b', 'c']
-
-
-
-// In the early days of Roman numerals, the Romans didn't bother with any of
-// this new-fangled subtraction 'IX' nonsense. No sir, it was straight addition,
-// biggest to littlest - so 9 was written 'VIIII' and so on.
-//
-// Define a function that takes in one argument
-//     num (number between 1 and 1000)
-//
-// Return a string that represents the roman numeral in the old school format.
-//
-// Example:
-//   oldSchoolRomanNumeral(9) === 'VIIII'
-//
-// TIP #1: Here's a mapping of Roman to Arabic numerals:
-//
-//   1000 = M
-//    500 = D
-//    100 = C
-//     50 = L
-//     10 = X
-//      5 = V
-//      1 = I
-//
-// TIP #2: Use the Math.floor and modulus methods will be helpful
-// TIP #3: You may like to use the repeat function that you defined above.
-
-
-
-
-// Eventually, someone thought it would be terribly clever if putting a smaller
-// number before a larger one meant you had to subtract the smaller one. As a
-// result of this development, you must now suffer.
-//
-// Rewrite your previous function so that, when passed any integer between 1 and
-// 1000
-// Return a string containing the proper modern Roman numeral.
-//
-// Example:
-//   newSchoolRomanNumeral(9) === 'IX'
-//
-// TIP #1: Here's a mapping of Roman to Arabic numerals:
-//
-//   1000 = M
-//    900 = CM
-//    500 = D
-//    400 = CD
-//    100 = C
-//     90 = XC
-//     50 = L
-//     40 = XL
-//     10 = X
-//      9 = IX
-//      5 = V
-//      4 = IV
-//      1 = I
-// TIP #3: You may like to use the repeat function that you defined above.
+// Return a new object that contains only the specified keys. For example, given
+// { name: 'moe', age: 50, id: 1 } and ['name', 'age'], then return
+// { name: 'moe', age: 50 }.
