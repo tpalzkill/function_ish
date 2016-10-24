95b3dc91a4748a1a9f497b732408127a4e8c5f1d
Ryan Sobol <contact@ryansobol.com>
2016-10-24 15:55:39 -0700

    Use codePointAt and fromCodePoint functions

--- part1/bonus.js
+++ part1/bonus.js
@@ -3,23 +3,23 @@ The purpose of this challenge is to be able to encode and decode a string using 
 
 https://en.wikipedia.org/wiki/Caesar_cipher
 
-Each string character has a numeric representation called an ASCII code. For example, the character "a" has an ASCII code of 97 and the character "b" has an ASCII code of 98.
+Each string character has a numeric representation called a Unicode value. For example, the character "a" has a Unicode value of 97 and the character "b" has an Unicode value of 98.
 
-https://en.wikipedia.org/wiki/ASCII#ASCII_printable_code_chart
+http://www.ssec.wisc.edu/~tomw/java/unicode.html#x0000
 
-JavaScript has a pair of methods to convert characters to and from ASCII codes.
+JavaScript has a pair of methods to convert characters to and from Unicode values.
 
-https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/charCodeAt
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/codePointAt
 
-https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/fromCharCode
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/fromCodePoint
 
 The Caesar cipher works like this.
 
-- Convert the character "a" to the ASCII code 97.
+- Convert the character "a" to the Unicode value 97.
 - Add 13 to that integer, resulting in 110.
-- Convert the new ASCII code to a character, resulting in "n".
+- Convert the new Unicode value to a character, resulting in "n".
 
-If by adding 13 to an ASCII code results in a number greater than 122 (i.e. "z"), wrap back around to the start of the alphabet.
+If adding 13 to a Unicode value results in a number greater than 122 (i.e. "z"), wrap back around to the start of the alphabet.
 
 It'll take a few steps to solve this cipher. Have fun!
 */
@@ -29,10 +29,13 @@ It'll take a few steps to solve this cipher. Have fun!
 //
 // If the integer is less than 97
 //    Throw an error with the message 'Error: integer too low'
+//
 // If the integer is greater than 122
 //    Throw an error with the message 'Error: integer too low'
-// If the integer + 13 would be greater than 122, the ASCII code for 'z'
+//
+// If the integer + 13 would be greater than 122, the Unicode value for 'z'
 //    Return an integer that is "wrapped" around
+//
 // Otherwise
 //    Return the integer plus 13
 //
@@ -48,14 +51,15 @@ function caesarShiftInt(integer) {
 //
 // If the string is not a lowercase letter
 //    Return the string as is
+//
 // Otherwise
 //    Return the char shifted using the caesarShiftInt() function
 //
 // For example, given '!', then return '!'. But given 'a' or 'A', return 'n'.
 //
-// See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/charCodeAt
+// See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/codePointAt
 //
-// See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/fromCharCode
+// See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/fromCodePoint
 
 
 
